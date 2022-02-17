54e1c9115785584cc00f689a4b3a70102754cd7b
Kamil Tulczyjew <7294494-ktulczyjew@users.noreply.gitlab.com>
2022-02-17 11:45:56 +0000

    Assets build preprocessor editor

--- Editor/Tools/SpriteDownloader/Window/Utils/AssetsRegexs.cs
+++ Editor/Tools/SpriteDownloader/Window/Utils/AssetsRegexs.cs
@@ -2,8 +2,9 @@
 {
     public static class AssetsRegexs
     {
-        public const string TEXTURE_REGEX = "[^\\s]+(.*?)\\.([jJ][pP][gG]|[jJ][pP][eE][gG]|[pP][nN][gG])$";
-        public const string BYTE_REGEX = "[^\\s]+(.*?)\\.([gG][iI][fF])$";
-        public const string CONFIG_REGEX = "[^\\s]+(.*?)\\.([jJ][sS][oO][nN])$";
+        public const string TEXTURE_REGEX = "[^\\s]+(.*?)([jJ][pP][gG]|[jJ][pP][eE][gG]|[pP][nN][gG])$";
+        public const string META_REGEX = "[^\\s]+(.*?)([mM][eE][tT][aA])$";
+        public const string BYTE_REGEX = "[^\\s]+(.*?)([gG][iI][fF])$";
+        public const string CONFIG_REGEX = "[^\\s]+(.*?)([jJ][sS][oO][nN])$";
     }
 }
